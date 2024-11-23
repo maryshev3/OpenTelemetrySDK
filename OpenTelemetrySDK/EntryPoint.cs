d2c9fe3460dba0d031a9915a760cf5858850bd32
maryshev3 <maryshev3@gmail.com>
2024-11-23 09:50:21 +0300

    first commit

---
+++ OpenTelemetrySDK/EntryPoint.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Exporter;
+using System.Reflection.PortableExecutable;
+using Microsoft.Extensions.Configuration;
+
+namespace OpenTelemetrySDK
+{
+    public class EntryPoint
+    {
+        public void InjectOpenTelemetryJager(IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection
+                .AddOpenTelemetry()
+                .WithTracing(builder =>
+                {
+                    builder.AddAspNetCoreInstrumentation();
+                    builder.AddJaegerExporter();
+                    builder.ConfigureServices(services => 
+                    {
+                        services.Configure<JaegerExporterOptions>(configuration.GetSection("Jaeger"));
+                    });
+                });
+        }
+    }
+}
