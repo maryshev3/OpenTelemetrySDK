49c4a6304e72e2e9914477f78779c1ef4d1513cf
Марышев Андрей Владимирович <maryshev_av@rtsoft.ru>
2024-12-21 22:04:45 +0300

    Правки

--- OpenTelemetrySDK/EntryPoint.cs
+++ OpenTelemetrySDK/EntryPoint.cs
@@ -10,24 +10,35 @@ using Microsoft.Extensions.DependencyInjection;
 using OpenTelemetry.Exporter;
 using Microsoft.Extensions.Configuration;
 using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Resources;
+using System.Diagnostics;
+using Npgsql;
 
 namespace OpenTelemetrySDK
 {
     public class EntryPoint
     {
-        public static void InjectOpenTelemetryJager(IServiceCollection serviceCollection, IConfiguration configuration)
+        public static void InjectOpenTelemetryJager(IServiceCollection serviceCollection, IConfiguration configuration, string applicationName, string[] activitySources)
         {
             serviceCollection
                 .AddOpenTelemetry()
                 .WithTracing(builder =>
                 {
+                    ActivitySource.AddActivityListener(new ActivityListener()
+                    {
+                        ShouldListenTo = _ => true,
+                        Sample = (ref ActivityCreationOptions<ActivityContext> _) => ActivitySamplingResult.AllDataAndRecorded,
+                    });
+                    
+                    builder.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(applicationName));
                     builder.SetSampler(new AlwaysOnSampler());
-
                     builder.AddHttpClientInstrumentation();
-
+                    builder.AddSource(activitySources);
+                    builder.AddNpgsql();
                     builder.AddAspNetCoreInstrumentation();
                     builder.AddJaegerExporter();
-                    builder.ConfigureServices(services => 
+                    builder.AddOtlpExporter(options => options.Endpoint = new Uri(configuration.GetValue<string>("OtlpExporterOptions:Endpoint") ?? string.Empty));
+                    builder.ConfigureServices(services =>
                     {
                         services.Configure<JaegerExporterOptions>(configuration.GetSection("Jaeger"));
                     });
